c0a9482e3d30c9309fa1856b1cc3176b166f3cf6
Espazam <wesleymmenezes@hotmail.com>
2023-12-04 12:08:02 -0300

    Desafio Finalizado

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 
 class Classes{
-    constructor(tipo, ataque){
-    
+    constructor(nome, idade, tipo, ataque){
+        this.nome = nome
+        this.idade = idade
         this.tipo = tipo
         this.ataque = ataque
     }
     atacar(){
-        console.log(`O ${this.tipo} atacou usando ${this.ataque}`)
+        console.log(`${this.nome}, o ${this.tipo} de ${this.idade} anos, atacou usando ${this.ataque}`)
     }
 }
 
-let mago = new Classes("mago", "magia")
-let guerreiro = new Classes("guerreiro", "espada")
-let monge = new Classes("monge", "artes marciais")
-let ninja = new Classes("ninja", "shuriken")
-    
+let mago = new Classes("Espazam", 595, "mago", "magia")
+let guerreiro = new Classes("Wellzeras", 100, "guerreiro", "espada")
+let monge = new Classes("LuckDay", 300, "monge", "artes marciais")
+let ninja = new Classes("Gaiden", 45, "ninja", "shuriken")
+
 mago.atacar()
 guerreiro.atacar()
 monge.atacar()
