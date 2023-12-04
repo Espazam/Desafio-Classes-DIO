ab14fc4a471ba6ef2d68e84fb30ad941b91b2690
Espazam <wesleymmenezes@hotmail.com>
2023-12-04 11:49:59 -0300

    desafio finalizado

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-asdsad
+
+class Classes{
+    constructor(tipo, ataque){
+    
+        this.tipo = tipo
+        this.ataque = ataque
+    }
+    atacar(){
+        console.log(`O ${this.tipo} atacou usando ${this.ataque}`)
+    }
+}
+
+let mago = new Classes("mago", "magia")
+let guerreiro = new Classes("guerreiro", "espada")
+let monge = new Classes("monge", "artes marciais")
+let ninja = new Classes("ninja", "shuriken")
+    
+mago.atacar()
+guerreiro.atacar()
+monge.atacar()
+ninja.atacar()
+    
